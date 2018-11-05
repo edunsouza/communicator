3dc816a2f540ae4cfddf5215c2b8b3863dbfd100
Eduardo Souza <eduardo.souza.ens@gmail.com>
2018-11-04 22:35:15 -0200

    remoção parcial de dados inúteis das frases do facebook

--- dataset/scrapper-playstore.js
+++ dataset/scrapper-playstore.js
@@ -1,5 +1,9 @@
 var classeComentario = "UD7Dzf";
 var holder = [];
-Array.from(new Set(document.getElementsByClassName(classeComentario))).forEach(x => holder.push(x.innerText));
+
+Array
+.from(document.getElementsByClassName(classeComentario))
+.forEach(x => holder.push(x.innerText));
+
 var set = new Set(holder);
 set.forEach(x => console.log(x));
